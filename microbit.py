9f2c070cddda33d21be6c0e0ed7614d989cd1d8e
erikbranom <44042588+erikbranom@users.noreply.github.com>
2018-11-04 23:04:31 -0700

    Add files via upload

---
+++ microbit.py
@@ -0,0 +1,36 @@
+from microbit import *
+import random
+
+if accelerometer.was_gesture("shake"):
+    set weapon display.show(str(random.randint(0, 4)))
+    
+    if weapon == 0
+    rock = Image("00000:"
+                 "09990:"
+                 "09990:"
+                 "09990:"
+                 "00000")
+    elif weapon == 1
+     paper = Image("99999:"
+                   "90009:"
+                   "90009:"
+                   "90009:"
+                   "99999")
+    elif weapon == 2
+    scissors = Image("99009:"
+                     "99090:"
+                     "00900:"
+                     "99090:"
+                     "99009")
+    elif weapon == 3
+    spock = Image("00900:"
+                  "09990:"
+                  "00900:"
+                  "09990:"
+                  "00900")
+    elif weapon ==4
+    lizard = Image("99099:"
+                   "99099:"
+                   "99099:"
+                   "99999:"
+                   "99999")
